f8d6db2ac2c025ac65bd23e06164933a57421841
CAM <camccuk@yahoo.com>
2013-08-14 21:59:07 +0000

    Added toString to avoid octet stream issue

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   buf = fs.readFileSync("index.html");
-  response.send(buf);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
